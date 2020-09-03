e4b3cb446f5ed7d5c4a3f69cf82b626de9540d33
Fernando Morani <morani.rightec@gmail.com>
2020-09-03 17:55:09 +0200

    Versione 1.0.6 - Animazione

--- assets/js/main.js
+++ assets/js/main.js
@@ -59,7 +59,74 @@ function addImgArticle(index, element){
 
 }
 
+function hideAllSession(){
+    let sessionCollect = document.getElementsByClassName("articleSession");
+        console.log("Sessioncollection");
+        for (singleSess of sessionCollect){
+            console.log(singleSess);
+            singleSess.style.display = "none";
+        }
+}
+
+function showAllSession() {
+    let start = 0;
+    let sessionCollect = document.getElementsByClassName("articleSession");
+    console.log("Sessioncollection");
+    function showSession() {
+        if (start < sessionCollect.length) {
+            console.log("Block n: ", start);
+            sessionCollect[start].style.display= "block";
+            start++;
+        } else {
+            clearAnimation();
+            //console.log("Clearing interval");
+        }
+    }
+    idInterval = setInterval(showSession, 3000);
+    console.log ("End showAllSession");
+
+/*for (singleSess of sessionCollect){
+    console.log(singleSess);
+    idInterval = setInterval(showSession, 3000, singleSess);
+}*/
+}
+
+function clearAnimation(){
+    //forcedHideButtonFlag = false;
+    clearInterval(idInterval);
+}
+/*
+function showSession(elem){
+    elem.style.display = "block";
+}
+*/
+
+
 function getCat(categIndex){
+
+    function stepArticleOrder(){
+        if (articleOrder < 3){
+            articleOrder++;
+        }else{
+            articleOrder = 0;
+        }
+    }
+    
+    function resetArticleOrder(){
+        articleOrder = -1;
+    }
+
+    function setTimeoutFlag(){
+        timeoutFlag = true;
+        console.log("setTimeoutFlag");
+    }
+
+    function resetTimeoutFlag(){
+        timeoutFlag = false;
+        console.log("resetTimeoutFlag");
+    }
+
+
     //console.log("getCat");
     const cPlainArticle = 0; //Identify a plain article (same as 2)
     const cMediaVideoArticle = 1; //Identify a media Article with video
@@ -69,18 +136,30 @@ function getCat(categIndex){
     let step_0_child = null;
     let step_session = null;
     let artSessChildNumb = 0;  //Store the number of session children
+    let timeoutFlag = false;
 
     let aCateg = new Array();
     let aDate = new Array(); //Array per ordinare gli articoli
 
     let numbItemToShow = 0; //Numero di articoli da mostrare
 
+    clearAnimation(); //In any case
+
     console.log("actualView is",actualView);
 
     if (currCategIndex != categIndex){
         currCategIndex = categIndex; //aggiorna la categoria corrente
         hideContButton(false); //riabilito il pulsante
+        if (buttonPressedFlag == true){
+            buttonPressedFlag = false; //reset del flag
+        }
+    }else{
+        if (buttonPressedFlag == true){
+            let hideBtn = document.getElementById("allNewsBlockId");
+            hideBtn.style.visibility = "hidden";       
+        }
     }
+    
 
     console.log("Current categopry is", currCategIndex);
 
@@ -125,20 +204,21 @@ function getCat(categIndex){
 
     console.log(aCateg);
     console.log(aDate);
+    //console.log("forcedHideButtonFlag is: ",forcedHideButtonFlag)
     
 
     //Cancella l'intera sessione
     artSessions.innerHTML="";
 
     //Per tutti gli elementi della categoria ordinati per data
-    for (let i=0; i<numbItemToShow;i++){
-        //Select teh more recent
-        tempArticle = getMoreRecent(aDate);
+    for (let i=0; i<numbItemToShow;i++){        
+        //Select the more recent        
+        tempArticle = getMoreRecent(aDate);           
         switch (articleOrder){  
             //State Machine         
             case -1:
-                // execute for 0
-                articleOrder = 0;
+                // execute for 0                                
+                stepArticleOrder();
                 addPlainArticle(aCateg[tempArticle].origIndex,artSessions); //Create the 1st child
                 step_0_child = artSessions.children[artSessChildNumb];  //position on the 1st child
                 step_session = artSessions.children[0];
@@ -147,35 +227,35 @@ function getCat(categIndex){
                 break;
             case 0:
                 // execute for 1
-                articleOrder = 1;
+                stepArticleOrder();
                 addVideoArticle(aCateg[tempArticle].origIndex, step_0_child);
                 console.log("step_0_child",step_0_child);
                 console.log("step_session",step_session);
                 break;
             case 1:
                 // execute for 2
-                articleOrder = 2;
+                stepArticleOrder();
                 addPlainArticle(aCateg[tempArticle].origIndex,artSessions);
                 artSessChildNumb++; //A new session has been added
                 step_0_child = artSessions.children[artSessChildNumb];  //.nextElementSibling;  //position on the 1st child
                 console.log("step_0_child",step_0_child);
-                console.log("step_session",step_session);                
+                console.log("step_session",step_session); 
                 break;            
             case 2:
                 // execute for 3
-                articleOrder = 3;
+                stepArticleOrder();
                 addImgArticle(aCateg[tempArticle].origIndex, step_0_child);
                 console.log("step_0_child",step_0_child);
-                console.log("step_session",step_session);                
+                console.log("step_session",step_session);
                 break;            
             case 3:
                 // execute for 0
-                articleOrder = 0;
+                stepArticleOrder();
                 addPlainArticle(aCateg[tempArticle].origIndex,artSessions);
                 artSessChildNumb++; //A new session has been added
                 step_0_child = artSessions.children[artSessChildNumb];  
                 console.log("step_0_child",step_0_child);
-                console.log("step_session",step_session);                
+                console.log("step_session",step_session);
                 break;            
             default:
                 //error
@@ -183,15 +263,22 @@ function getCat(categIndex){
                 break;
         }//end of switch
     }//end of for
-    articleOrder = -1; //Reset the state machine
+    resetArticleOrder(); //Reset the state machine
+    if ((getContButtonVisib() == "hidden") && 
+        (buttonPressedFlag == true))  {
+        hideAllSession();
+        showAllSession();
+        buttonPressedFlag = false;
+    }
 }//end of function
 
 function hideContButton(hide){
     let hideBtn = document.getElementById("allNewsBlockId");
     if (hide == true){
         //hide continue button
-        hideBtn.style.visibility = "hidden";
+        //hideBtn.style.visibility = "hidden";
         actualView = cMaxArticleShow;
+        buttonPressedFlag = true;
         getCat(currCategIndex);
     }else{
         //show continue button
@@ -208,6 +295,11 @@ function forceContButtonVisib(){
 function forceContButtonHide(){
     let hideBtn = document.getElementById("allNewsBlockId");
     hideBtn.style.visibility = "hidden"; 
+}
+
+function getContButtonVisib(){
+    let hideBtn = document.getElementById("allNewsBlockId");
+    return hideBtn.style.visibility; 
 }
 
 //Imposto le costanti
@@ -228,6 +320,8 @@ let artSessions = document.getElementById("fullSessionId");
 let allNewsButton = document.getElementById("allNewsBtnId");    //il button "continua"
 let actualView = defViews;                                      //NUmber of article to show
 let currCategIndex = -1;
+let idInterval = -1;
+let buttonPressedFlag = false;
 
 
 if (buttonCnt == aCategories.length){
